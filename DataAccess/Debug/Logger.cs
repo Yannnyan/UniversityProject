71a3b4eb7986e098026c30874aac2c748870df92
Yan <regoner2@gmail.com>
2022-08-30 09:46:55 +0300

    log files

---
+++ DataAccess/Debug/Logger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversityProject.Debug
+{
+    public class Logger
+    {
+        private static string path = @"C:\Users\Yan\Desktop\Projects\database_projects\first_project\UniversityProject\Debug\logFile.txt";
+
+        public static void Log(string value)
+        {
+            using (StreamWriter fs = File.AppendText(path))
+            {
+                fs.WriteLine(value);
+            } 
+        }
+    }
+}
